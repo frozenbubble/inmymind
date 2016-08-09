7e0238e9ada742651ba40039bb6d509b128af43f
Péter Szűcs <Pter Szcs>
2016-08-10 00:40:27 +0200

    freely draggable nodes

--- src/noteeditor.component.ts
+++ src/noteeditor.component.ts
@@ -1,14 +1,28 @@
 import { Component } from 'angular2/core';
 
+import { NodeComponent } from './node.component';
+
 @Component({
     selector: 'note-editor',
     template: `
         <div id="page-content-wrapper">
             <div class="container-fluid">
-                editor
+                <div class="note-content" (drop)="drop($event)" (dragover)="dragover($event)">
+                    <node></node>
+                </div>
             </div>
-        </div>`
+        </div>`, 
+    directives: [NodeComponent]
 })
-export class NoteEditorComponent {
+export class NoteEditorComponent 
+{
+    drop(event: DragEvent) {
+        event.preventDefault();
+        return false;
+    }
 
+    dragover() {
+        event.preventDefault();
+        return false;
+    }
 }
