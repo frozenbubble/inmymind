c20e2772d0853b3b57c41e874cacb9e8cadbeead
Péter Szűcs <Pter Szcs>
2016-08-18 20:07:56 +0200

    add button lights up on hover and click

--- src/notepicker.component.ts
+++ src/notepicker.component.ts
@@ -8,11 +8,11 @@ import { Category, Map } from './notemodel';
 @Component({
     selector: 'notepicker',
     template: `
-        <nav class="nav-group" (mouseover)="showAdd(true)" (mouseout)="showAdd(false)">
-            <h5 class="nav-group-title category-title" (click)="toggle()">
+        <nav class="nav-group" (mouseenter)="showAdd(true)" (mouseleave)="showAdd(false)">
+            <h5 class="nav-group-title category-title" (click)="toggle($event)">
                 <span class="icon" [class.icon-down-open]="opened" [class.icon-right-open]="!opened"></span>
                     {{category.title}}
-                <span *ngIf="inFocus" class="icon icon-plus pull-right"></span>
+                <span *ngIf="inFocus" (click)="addMap($event)" class="icon icon-plus-circled pull-right picker-button"></span>
             </h5>
             <div *ngIf="opened">
                 <span class="nav-group-item" (click)="select(m)" *ngFor="let m of category.maps" [class.active]="m.id === selectedMap.id">
@@ -25,6 +25,7 @@ export class NotePickerComponent {
     @Input() category: Category;
     @Input() selectedMap: Map = new Map();
     @Output() onSelect = new EventEmitter<Map>();
+    @Output() onNewMap = new EventEmitter<Map>();
     
     private opened: boolean = false;
     private inFocus = false;
@@ -32,14 +33,32 @@ export class NotePickerComponent {
     select(m: Map) {
         this.selectedMap = m;
         this.onSelect.emit(m);
+        console.log(m);
     }
 
-    toggle() {
-        this.opened = !this.opened;
+    toggle(event: MouseEvent) {
+        let target = event.target as Element;
+
+        if (!target.className.includes('picker-button')) {
+            this.opened = !this.opened;
+        } else {
+            this.opened = true;
+            // target.parentElement.appendChild
+        }
+
+        
+
     }
 
     showAdd(value: boolean) {
         this.inFocus = value;
-        console.log(value);
+    }
+
+    addMap(event: MouseEvent) {
+        // event.stopPropagation();
+        // this.opened = true;
+
+        // let target = event.target as Element;       
+        // console.log(target.className);
     }
 }
