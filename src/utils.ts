924938fc71a85c5ac503cf8b5149cad9e682b9ac
Peter Szucs <pszucs@outlook.com>
2017-02-12 17:14:46 +0100

    upgrading to zkit api v4

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,35 @@
 export class Utils {
     static apiUrl: string = "http://localhost:3000";
 
-    static callServer (urlPart, obj){
-        var url = this.apiUrl + '/api/' + urlPart;
-        console.log(url);
-        return new Promise(function (resolve, reject) {
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', url);
-            xhr.onload = function () {
-                var me = <any>this;
-                if (me.status >= 200 && me.status < 300) {
-                    resolve(JSON.parse(xhr.responseText || "null"));
-                } else {
+    static callServer (urlPart, obj, resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', this.apiUrl + '/api/' + urlPart);
+        xhr.onload = function () {
+            if ((<any>this).status >= 200 && (<any>this).status < 300) {
+                if (resolve) {
+                    resolve(JSON.parse(xhr.responseText || 'null'));
+                }
+            } else {
+                if (reject) {
+                    console.log('bad status code');
+
                     reject({
-                    status: me.status,
-                    statusText: xhr.statusText
+                        status: (<any>this).status,
+                        statusText: xhr.statusText
                     });
                 }
-            };
-
-            xhr.onerror = function () {
+            }
+        };
+        xhr.onerror = function () {
+            console.log('xhr onerror');
+            if (reject) {
                 reject({
                     status: (<any>this).status,
                     statusText: xhr.statusText
                 });
-            };
-            
-            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-            xhr.send(JSON.stringify(obj));
-        });
-    }
+            }
+        };
+        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+        xhr.send(JSON.stringify(obj));
+    };
 }
--- src/zkit.service.ts
+++ src/zkit.service.ts
@@ -4,32 +4,32 @@ declare var zkit_sdk: any;
 
 export class ZkitService {
     constructor () {
-        zkit_sdk.setup("https://i00227bj31.api.tresorit.io");
+        zkit_sdk.setup("https://host-s9m5bg49sm.api.tresorit.io", "/tenant-vocvlgu3u4");
     }
 
     register(frame, alias) {
         var userIdResp;
 
+        console.log('initialising user registration');
         // Step 1: reserve a unique user ID for the registration
-        return Utils.callServer('init-user-reg', {alias: alias}).then(function(resp){
-            userIdResp = resp;
-
-            // Step 2: register user
-            return frame.register(userIdResp.userId, userIdResp.regSessionId);
-        }).then(function(succRegResp){
-
-            // Step 3: finish registration transaction
-            //   (and auto-approve user for demo purposes)
-            Utils.callServer("finished-registration", {
-                userId: userIdResp.userId,
-                regValidationVerifier: succRegResp.RegValidationVerifier
+        return Utils.callServer('init-user-reg', { alias: alias }, userIdResp => { 
+            debugger;           
+            // Step 2: register user through the sdk
+            return frame.register(userIdResp.userId, userIdResp.regSessionId).then(succRegResp => {
+                // Step 3: finish registration transaction
+                // (and auto-approve user for demo purposes)
+                Utils.callServer("finished-registration", {
+                    userId: userIdResp.userId,
+                    regValidationVerifier: succRegResp.RegValidationVerifier
+                },  () => {
+                    console.log('Registration successful');
+                }, error => {
+                    console.log(`error: ${error}`);
+                });
             });
-
-
-            console.log('REGISTERED')
-            // TODO
-            // showPanel('nextStep');
-
+        }, error => {
+            console.log('error:');
+            console.log(error);
         });
     }
 }
